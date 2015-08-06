63654f097fe1d6227ed11206d8b9c3f9a02b66c7
Tomek Drabas <drabas.t@gmail.com>
2015-08-06 15:39:16 +1000

    Added XML parser code: read_xml.py

--- Codes/Chapter1/read_xml.py
+++ Codes/Chapter1/read_xml.py
@@ -2,8 +2,14 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 
 def xml_encode(row):
+    '''
+        Encode the row as an XML with a specific hierarchy
+    '''
+    # first -- we are writing a record
     xmlItem = ['  <record>']
 
+    # next -- for each field in the row we create a XML markup
+    #         in a <field name=...>...</field> format
     for field in row.index:
         xmlItem \
             .append(
@@ -11,8 +17,33 @@ def xml_encode(row):
                 .format(field, row[field])
             )
     
+    # last -- this marks the end of the record
     xmlItem.append('  </record>')
+
+    # return a string back to the calling method
     return '\n'.join(xmlItem)
+
+def write_xml(xmlFileName, data):
+    '''
+        Save the data in an XML format
+    '''
+    # open the file for writing
+    with open(xmlFileName, 'w') as xmlFile:
+
+        # write the headers
+        xmlFile.write(
+            '<?xml version="1.0" encoding="UTF-8"?>\n'
+        )
+
+        xmlFile.write('<records>\n')
+
+        # write the data
+        xmlFile.write(
+            '\n'.join(data.apply(xml_encode, axis=1))
+        )
+
+        # write the footer
+        xmlFile.write('\n</records>')
 
 def iter_records(records):
     '''
@@ -29,19 +60,29 @@ def iter_records(records):
         # generate the value
         yield return_dict
 
-def read_xml(xml_tree):
-    tree = ET.parse(xml_tree)
-    root = tree.getroot()
+def read_xml(xmlFileName):
+    '''
+        Read an XML encoded data and return pd.DataFrame
+    '''
+    with open(xmlFileName, 'r') as xml_file:
+        # read the data and store it as a tree
+        tree = ET.parse(xml_file)
 
-    return pd.DataFrame(list(iter_records(root)))
+        # get the root of the tree as this is the starting point
+        root = tree.getroot()
 
-# names of files to read from
+        # return the DataFrame
+        return pd.DataFrame(list(iter_records(root)))
+
+# names of files to read from and write to
 r_filenameXML = '../../Data/Chapter1/realEstate_trans.xml'
+w_filenameXML = '../../Data/Chapter1/realEstate_trans.xml'
 
 # read the data
-with open(r_filenameXML, 'r') as xml_file:
-    xml_read = read_xml(xml_file)
+xml_read = read_xml(r_filenameXML)
 
 # print the first 10 records
 print(xml_read.head(10))
 
+# write back to the file in an XML format
+write_xml(w_filenameXML, xml_read)
