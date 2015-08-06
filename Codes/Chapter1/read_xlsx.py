edfac6f07dafc941d91d5ba09810f2ec5a6f01d0
Tomek Drabas <drabas.t@gmail.com>
2015-08-06 10:55:41 +1000

    Added writing to XLSX to read_xlsx.py

--- Codes/Chapter1/read_xlsx.py
+++ Codes/Chapter1/read_xlsx.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 # name of files to read from
 r_filenameXLSX = '../../Data/Chapter1/realEstate_trans.xlsx'
+w_filenameXLSX = '../../Data/Chapter1/realEstate_trans.xlsx'
 
 # sheet name(s)
 sheets = ['Sacramento']
@@ -17,3 +18,7 @@ xlsx_read = {
 
 # print the first 10 records for Sacramento
 print(xlsx_read['Sacramento'].head(10))
+
+# write to Excel
+xlsx_read['Sacramento'] \
+    .to_excel(w_filenameXLSX, 'Sacramento', index=False)
