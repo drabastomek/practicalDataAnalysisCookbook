8219eb03b8310ce224e7d7b18f97f549a1e4bc52
U-REDMOND\todrabas <todrabas@tomd-desktop.redmond.corp.microsoft.com>
2015-08-20 13:07:02 -0700

    Imputing mean in place of NaNs for price

---
+++ Codes/Chapter1/data_impute.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+
+# name of the file to read from
+r_filenameCSV = '../../Data/Chapter1/realEstate_trans_dirty.csv'
+
+# name of the output file
+w_filenameCSV = '../../Data/Chapter1/realEstate_trans_price_imputed.csv'
+
+# read the data
+csv_read = pd.read_csv(r_filenameCSV)
+
+# impute mean in place of NaNs
+csv_read["price"].fillna(csv_read.groupby("city_state_zip")["price"].transform("mean"), inplace=True)
+
+# and write to a file
+with open(w_filenameCSV, 'w') as write_csv:
+	write_csv.write(csv_read.to_csv(sep=',', index=False))
