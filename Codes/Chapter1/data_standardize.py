adf6218336d79d7d06c747b0519c0cec7c456228
Tomek Drabas <drabas.t@gmail.com>
2015-08-20 20:51:12 -0700

    Added standardization and normalization code

---
+++ Codes/Chapter1/data_standardize.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+def standardize(col):
+    '''
+        Standardize column
+    '''
+    return (col - col.mean()) / col.std()
+
+def normalize(col):
+    '''
+        Normalize column
+    '''
+    return (col - col.min()) / (col.max() - col.min())
+
+# name of the file to read from
+r_filenameCSV = '../../Data/Chapter1/' + \
+    'realEstate_trans_price_imputed.csv'
+
+# name of the output file
+w_filenameCSV = '../../Data/Chapter1/' + \
+    'realEstate_trans_standardized.csv'
+
+# list of columns to normalize and standardize
+cols = ['price','sq__ft']
+
+# read the data
+csv_read = pd.read_csv(r_filenameCSV)
+
+# impute mean in place of NaNs
+for col in cols:
+    csv_read[col + '_s'] = standardize(csv_read[col])
+    csv_read[col + '_n'] = normalize(csv_read[col])
+
+# check if all is well now
+for col in cols:
+    print('{0}_s: mean -- {1:.2f}, std dev -- {2:.2f}' \
+        .format(
+            col, 
+            csv_read[col + '_s'].mean(),
+            csv_read[col + '_s'].std()
+        )
+    )
+
+    print('{0}_n: min -- {1:.2f}, max -- {2:.2f}\n' \
+        .format(
+            col,
+            csv_read[col + '_n'].min(),
+            csv_read[col + '_n'].max()
+        )
+    )
+
+# and write to a file
+with open(w_filenameCSV, 'w') as write_csv:
+	write_csv.write(csv_read.to_csv(sep=',', index=False))
