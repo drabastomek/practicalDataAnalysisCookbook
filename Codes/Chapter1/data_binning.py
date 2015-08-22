c2aced07f05f55bbef7df08aa087e631d84f6b0f
Tomek Drabas <drabas.t@gmail.com>
2015-08-21 19:17:23 -0700

    Altered the price to price_mean

--- Codes/Chapter1/data_binning.py
+++ Codes/Chapter1/data_binning.py
@@ -15,23 +15,29 @@ csv_read = pd.read_csv(r_filenameCSV)
 # create bins for the price that are based on the 
 # linearly spaced range of the price values
 bins = np.linspace(
-    csv_read['price'].min(), 
-    csv_read['price'].max(), 
+    csv_read['price_mean'].min(), 
+    csv_read['price_mean'].max(), 
     6
 )
 
 # and apply the bins to the data
-csv_read['b_price'] = np.digitize(csv_read['price'], bins)
+csv_read['b_price'] = np.digitize(
+    csv_read['price_mean'], 
+    bins
+)
 
 # print out the counts for the bins
 counts_b = csv_read['b_price'].value_counts()
 print(counts_b.sort_index())
 
 # create bins based on percentiles
-perc = csv_read['price'].quantile(np.linspace(0,1,11))
+perc = csv_read['price_mean'].quantile(np.linspace(0,1,11))
 
 # and apply the percentile bins to the data
-csv_read['p_price'] = np.digitize(csv_read['price'], perc)
+csv_read['p_price'] = np.digitize(
+    csv_read['price_mean'], 
+    perc
+)
 
 # print out the counts for the percentile bins
 counts_p = csv_read['p_price'].value_counts()
