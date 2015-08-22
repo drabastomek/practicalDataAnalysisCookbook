55b9a5c8817828c98d6da6f3993202d2de675250
Tomek Drabas <drabas.t@gmail.com>
2015-08-21 20:02:24 -0700

    Changed the order of methods

--- Codes/Chapter1/data_standardize.py
+++ Codes/Chapter1/data_standardize.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-def standardize(col):
-    '''
-        Standardize column
-    '''
-    return (col - col.mean()) / col.std()
-
 def normalize(col):
     '''
         Normalize column
     '''
     return (col - col.min()) / (col.max() - col.min())
+
+def standardize(col):
+    '''
+        Standardize column
+    '''
+    return (col - col.mean()) / col.std()
 
 # name of the file to read from
 r_filenameCSV = '../../Data/Chapter1/' + \
@@ -28,24 +28,24 @@ csv_read = pd.read_csv(r_filenameCSV)
 
 # impute mean in place of NaNs
 for col in cols:
-    csv_read['s_' + col] = standardize(csv_read[col])
     csv_read['n_' + col] = normalize(csv_read[col])
+    csv_read['s_' + col] = standardize(csv_read[col])
 
 # check if all is well now
 for col in cols:
-    print('{0}_s: mean -- {1:.2f}, std dev -- {2:.2f}' \
-        .format(
-            col, 
-            csv_read['s_' + col].mean(),
-            csv_read['s_' + col].std()
-        )
-    )
-
     print('{0}_n: min -- {1:.2f}, max -- {2:.2f}\n' \
         .format(
             col,
             csv_read['n_' + col].min(),
             csv_read['n_' + col].max()
+        )
+    )
+
+    print('{0}_s: mean -- {1:.2f}, std dev -- {2:.2f}' \
+        .format(
+            col, 
+            csv_read['s_' + col].mean(),
+            csv_read['s_' + col].std()
         )
     )
 
