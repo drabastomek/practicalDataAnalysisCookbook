405b1f4e6d33164cf6e4ee9b0f2f02ec7c262765
Tomek Drabas <drabas.t@gmail.com>
2015-08-21 19:14:55 -0700

    Added median imputation

--- Codes/Chapter1/data_impute.py
+++ Codes/Chapter1/data_impute.py
@@ -12,7 +12,16 @@ w_filenameCSV = '../../Data/Chapter1/' + \
 csv_read = pd.read_csv(r_filenameCSV)
 
 # impute mean in place of NaNs
-csv_read["price"].fillna(csv_read.groupby("zip")["price"].transform("mean"), inplace=True)
+csv_read['price_mean'] = csv_read['price'] \
+    .fillna(
+        csv_read.groupby('zip')['price'].transform('mean')
+    )
+
+# impute median in place of NaNs
+csv_read['price_median'] = csv_read['price'] \
+    .fillna(
+        csv_read.groupby('zip')['price'].transform('median')
+    )
 
 # and write to a file
 with open(w_filenameCSV, 'w') as write_csv:
