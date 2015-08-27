a9e4d9f4a6d69f6de84575c8ff03d7c952965a2f
Tomek Drabas <drabas.t@gmail.com>
2015-08-26 20:55:17 -0700

    Added alternative way of calculating (not all) statistics

--- Codes/Chapter2/data_describe_alternative.py
+++ Codes/Chapter2/data_describe_alternative.py
@@ -1,39 +1,21 @@
 import scipy.stats as st
 import numpy as np
-import csv
 
 # name of the file to read from
 r_filenameCSV = '../../Data/Chapter2/' + \
     'realEstate_trans_full.csv'
-
-# name of the output file
-w_filenameCSV = '../../Data/Chapter2/' + \
-    'realEstate_descriptives.csv'
 
 # read the data
 csv_read = np.genfromtxt(
     r_filenameCSV, 
     delimiter=',',
     names=True,
-    excludelist=['street','city','state','zip','date','latitude','longitude']
+    # only numeric columns
+    usecols=[4,5,6,8,11,12,13,14,15,16,17,18,19,20]
 )
 
-# print(st.describe(csv_read, axis=1))
-print(csv_read)
+# calculate the descriptives
+desc = st.describe([list(item) for item in csv_read])
 
-
-# csv_read = pd.read_csv(r_filenameCSV)
-
-# # calculate the descriptives: count, mean, std,
-# # min, 25%, 50%, 75%, max
-# csv_desc = csv_read.describe().transpose()
-
-# # and add skewness, mode and kurtosis
-# csv_desc['skew'] = csv_read.skew(numeric_only=True)
-# csv_desc['mode'] = \
-#     csv_read.mode(numeric_only=True).transpose()
-# csv_desc['kurtosis'] = csv_read.kurt(numeric_only=True)
-
-# # output the descriptives to a file
-# with open(w_filenameCSV,'w') as write_csv:
-#     write_csv.write(csv_desc.to_csv(sep=','))
+# and print out to the screen
+print(desc)
