0ccbc6050a52b2ecfd1ae8cd8a05d6e35b8b5570
Tomek Drabas <drabas.t@gmail.com>
2015-08-26 21:21:54 -0700

    Added script to calculate correlations

---
+++ Codes/Chapter2/data_correlations.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+
+# name of the file to read from
+r_filenameCSV = '../../Data/Chapter2/' + \
+    'realEstate_trans_full.csv'
+
+# name of the output file
+w_filenameCSV = '../../Data/Chapter2/' + \
+    'realEstate_corellations.csv'
+
+# read the data and select only 4 variables
+csv_read = pd.read_csv(r_filenameCSV)
+csv_read = csv_read[['beds','baths','sq__ft','price']]
+
+# calculate the correlations
+methods = ['pearson', 'kendall', 'spearman']
+
+csv_corr = {}
+
+for method in methods:
+    csv_corr[method] = csv_read \
+        .corr(method=method) \
+        .transpose()
+
+# output to a file
+with open(w_filenameCSV,'w') as write_csv:
+    for corr in csv_corr:
+        write_csv.write(corr + '\n')
+        write_csv.write(csv_corr[corr].to_csv(sep=','))
+        write_csv.write('\n')
