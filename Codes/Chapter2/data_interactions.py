086dae7933ed8ae7aea04c63028a3f0131064d70
Tomek Drabas <drabas.t@gmail.com>
2015-08-31 09:13:18 -0700

    Prepping for D3 scatter plot visualization

---
+++ Codes/Chapter2/data_interactions.py
@@ -0,0 +1,25 @@
+# import numpy as np
+import pandas as pd
+import sqlalchemy as sa
+# import sklearn.cross_validation as sk
+
+# names of the files to output the samples
+w_filenameD3js = '../../Data/Chapter2/realEstate_d3.csv'
+
+# database credentials
+usr  = 'drabast'
+pswd = 'pAck7!B0ok'
+
+# create the connection to the database
+engine = sa.create_engine(
+    'postgresql://{0}:{1}@localhost:5432/{0}' \
+    .format(usr, pswd)
+)
+
+# read prices from the database
+query = 'SELECT beds, sq__ft, price FROM real_estate'
+data = pd.read_sql_query(query, engine)
+
+# output the samples to files
+with open(w_filenameD3js,'w') as write_csv:
+    write_csv.write(data.to_csv(sep=',', index=False))
