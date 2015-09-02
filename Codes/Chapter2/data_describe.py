e77a79d9859729df3940cabbbff1ca194e5279e4
Tomek Drabas <drabas.t@gmail.com>
2015-09-01 21:04:41 -0700

    Changed the name of the price variable

--- Codes/Chapter2/data_describe.py
+++ Codes/Chapter2/data_describe.py
@@ -16,19 +16,20 @@ csv_read = pd.read_csv(r_filenameCSV)
 # for a subset of columns
 csv_desc = csv_read[
     [   
-        'beds','baths','sq__ft','price','s_price',
-        'n_price','s_sq__ft','n_sq__ft','b_price',
+        'beds','baths','sq__ft','price','s_price_mean',
+        'n_price_mean','s_sq__ft','n_sq__ft','b_price',
         'p_price','d_Condo','d_Multi-Family',
         'd_Residential','d_Unkown'
     ]
-].describe().transpose()
+].describe()#.transpose()
+print(csv_desc)
 
-# and add skewness, mode and kurtosis
-csv_desc['skew'] = csv_read.skew(numeric_only=True)
-csv_desc['mode'] = \
-    csv_read.mode(numeric_only=True).transpose()
-csv_desc['kurtosis'] = csv_read.kurt(numeric_only=True)
+# # and add skewness, mode and kurtosis
+# csv_desc['skew'] = csv_read.skew(numeric_only=True)
+# csv_desc['mode'] = \
+#     csv_read.mode(numeric_only=True).transpose()
+# csv_desc['kurtosis'] = csv_read.kurt(numeric_only=True)
 
-# output the descriptives to a file
-with open(w_filenameCSV,'w') as write_csv:
-    write_csv.write(csv_desc.to_csv(sep=','))
+# # output the descriptives to a file
+# with open(w_filenameCSV,'w') as write_csv:
+#     write_csv.write(csv_desc.to_csv(sep=','))
