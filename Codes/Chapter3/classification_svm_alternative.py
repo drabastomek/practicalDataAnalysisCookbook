d0a7a6c5d034be4ff93728b8db6aa3c42f2dd659
Tomek Drabas <drabas.t@gmail.com>
2015-09-13 13:46:44 -0700

    Alternative way of classifying calls with SVM

---
+++ Codes/Chapter3/classification_svm_alternative.py
@@ -0,0 +1,42 @@
+# this is needed to load helper from the parent folder
+import sys
+sys.path.append('..')
+
+# the rest of the imports
+import helper as hlp
+import pandas as pd
+import sklearn.svm as sv
+
+@hlp.timeit
+def fitSVM(data):
+    '''
+        Build the SVM classifier
+    '''
+    # create the classifier object
+    svm = sv.SVC(kernel='linear', C=1.0)
+
+    # fit the data
+    return svm.fit(data[0],data[1])
+
+# the file name of the dataset
+r_filename = '../../Data/Chapter3/bank_contacts.csv'
+
+# read the data
+csv_read = pd.read_csv(r_filename)
+
+# split the data into training and testing
+train_x, train_y, \
+test_x,  test_y, \
+labels = hlp.split_data(
+    csv_read, 
+    y = 'credit_application'
+)
+
+# train the model
+classifier = fitSVM((train_x, train_y))
+
+# classify the unseen data
+predicted = classifier.predict(test_x)
+
+# print out the results
+hlp.printModelSummary(test_y, predicted)
