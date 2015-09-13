5ee49b4a5c60b0c005ad12d19dae814243a91198
Tomek Drabas <drabas.t@gmail.com>
2015-09-12 18:20:27 -0700

    Code cleanup

--- Codes/Chapter3/classification_logistic.py
+++ Codes/Chapter3/classification_logistic.py
@@ -5,22 +5,7 @@ sys.path.append('..')
 # the rest of the imports
 import helper as hlp
 import pandas as pd
-# import sklearn.linear_model as lm
-# import sklearn.cross_validation as cv
-
 import statsmodels.api as sm
-
-# data = sm.datasets.scotland.load()
-# # print(data.exog)
-
-# # data.exog = sm.add_constant(data.exog)
-
-# # print(data)
-# gamma_model = sm.GLM(data.endog, data.exog, family=sm.families.Gamma())
-# gamma_results = gamma_model.fit()
-
-# print(gamma_results.params)
-# print(gamma_results.pvalues)
 
 @hlp.timeit
 def fitLogisticRegression(data):
