534ce2f89459f4c1578217ff2c25fb1cc1f327ea
Tomek Drabas <drabas.t@gmail.com>
2015-10-18 13:03:32 -0700

    Moved the plotting method to helper.py and cleaned up the code

--- Codes/Chapter5/reduce_pca.py
+++ Codes/Chapter5/reduce_pca.py
@@ -6,17 +6,21 @@ sys.path.append('..')
 import helper as hlp
 import pandas as pd
 import numpy as np
-
 import mlpy as ml
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 @hlp.timeit
 def reduce_PCA(x):
+    '''
+        Reduce the dimensions using Principal Component
+        Analysis 
+    '''
+    # create the PCA object
     pca = ml.PCA(whiten=True)
+
+    # learn the principal components from all the features
     pca.learn(x)
 
+    # return only 3 principal components
     return pca.transform(x, k=3)
 
 # the file name of the dataset
@@ -25,38 +29,17 @@ r_filename = '../../Data/Chapter5/bank_contacts.csv'
 # read the data
 csv_read = pd.read_csv(r_filename)
 
+# split into independent and dependent features
 x = csv_read[csv_read.columns[:-1]]
 y = csv_read[csv_read.columns[-1]]
-y_np = np.array(y)
 
+# reduce the dimensionality
 z = reduce_PCA(x)
 
-# print(y.unique())
+# save the chart
+file_save_params = {
+    'filename': '../../Data/Chapter5/charts/pca_3d.png', 
+    'dpi': 300
+}
 
-# plt.set_cmap(plt.cm.Paired)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-color_marker = [('r','o'),('g','^'),('b','.')]
-
-for i in range(0, len(y.unique())):
-    ax.scatter(
-        z[y_np == i, 0], 
-        z[y_np == i, 1], 
-        z[y_np == i, 2], 
-        c=color_marker[i][0], 
-        marker=color_marker[i][1])
-
-    # print(i,z[y[y == i], 0], 
-    #     z[y[y == i], 1], 
-    #     z[y[y == i], 2], 
-    #     color_marker[i][0], 
-    #     color_marker[i][1])
-
-
-# print(z[0:3], z[y_np == 0, 0])
-
-# plt.show()
-plt.savefig('../../Data/Chapter5/charts/pca_3d.png', dpi=300)
-
-# print(csv_read.columns[:-1])
+hlp.plot_components(z, y, **file_save_params)
