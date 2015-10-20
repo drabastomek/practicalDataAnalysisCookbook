6315f4f17af21028ed25171b136b99b295dd0677
Tomek Drabas <drabas.t@gmail.com>
2015-10-19 21:08:24 -0700

    Using Linear Discriminant Analysis to classify calls

--- Codes/Chapter5/reduce_LDA.py
+++ Codes/Chapter5/reduce_LDA.py
@@ -22,6 +22,21 @@ def reduce_LDA(x, y):
 
     return lda
 
+@hlp.timeit
+def fitLinearSVM(data):
+    '''
+        Build the linear SVM classifier
+    '''
+    # create the classifier object
+    svm = ml.LibSvm(svm_type='c_svc', 
+        kernel_type='linear', C=20.0)
+
+    # fit the data
+    svm.learn(data[0],data[1])
+
+    # return the classifier
+    return svm
+
 # the file name of the dataset
 r_filename = '../../Data/Chapter5/bank_contacts.csv'
 
@@ -33,4 +48,22 @@ x = csv_read[csv_read.columns[:-1]]
 y = csv_read[csv_read.columns[-1]]
 
 # reduce the dimensionality
-z = reduce_LDA(x, y)
+csv_read['reduced'] = reduce_LDA(x, y).transform(x)
+
+# split the data into training and testing
+train_x, train_y, \
+test_x,  test_y, \
+labels = hlp.split_data(
+    csv_read, 
+    y = 'credit_application',
+    x = ['reduced']
+)
+
+# train the model
+classifier_l = fitLinearSVM((train_x, train_y))
+
+# classify the unseen data
+predicted_l = classifier_l.pred(test_x)
+
+# print out the results
+hlp.printModelSummary(test_y, predicted_l)
