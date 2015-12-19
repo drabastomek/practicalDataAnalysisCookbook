6e902c4cf1c1523d5a9de792ab23bf4c0ca5b767
Tomek Drabas <drabas.t@gmail.com>
2015-12-19 15:19:49 -0800

    Added some measures

--- Codes/Chapter8/graph_handling.py
+++ Codes/Chapter8/graph_handling.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import networkx.algorithms as alg
+import numpy as np
 import matplotlib.pyplot as plt
 
 # create graph object
@@ -27,49 +29,50 @@ twitter.add_node('Markus', {'age': 21})
 twitter.add_node('Glenn', {'age': 24})
 
 # add followers
-twitter.add_edge('Rob', 'Rory', {'weight': 1})
-twitter.add_edge('Markus', 'Rory', {'weight': 1})
-twitter.add_edge('Markus', 'Rob', {'weight': 1})
-twitter.add_edge('Mike', 'Rory', {'weight': 1})
-twitter.add_edge('Mike', 'Rob', {'weight': 1})
-twitter.add_edge('Mike', 'Markus', {'weight': 1})
-twitter.add_edge('Mike', 'Linda', {'weight': 1})
-twitter.add_edge('Alex', 'Rob', {'weight': 1})
-twitter.add_edge('Alex', 'Markus', {'weight': 1})
-twitter.add_edge('Alex', 'Linda', {'weight': 1})
-twitter.add_edge('Alex', 'Mike', {'weight': 1})
-twitter.add_edge('Glenn', 'Rory', {'weight': 1})
-twitter.add_edge('Glenn', 'Rob', {'weight': 1})
-twitter.add_edge('Glenn', 'Markus', {'weight': 1})
-twitter.add_edge('Glenn', 'Linda', {'weight': 1})
-twitter.add_edge('Glenn', 'Mike', {'weight': 1})
-twitter.add_edge('Glenn', 'Alex', {'weight': 1})
-twitter.add_edge('Richard', 'Rob', {'weight': 1})
-twitter.add_edge('Richard', 'Linda', {'weight': 1})
-twitter.add_edge('Richard', 'Mike', {'weight': 1})
-twitter.add_edge('Richard', 'Alex', {'weight': 1})
-twitter.add_edge('Richard', 'Glenn', {'weight': 1})
-twitter.add_edge('Jackie', 'Linda', {'weight': 1})
-twitter.add_edge('Jackie', 'Mike', {'weight': 1})
-twitter.add_edge('Jackie', 'Glenn', {'weight': 1})
-twitter.add_edge('Tom', 'Rachel', {'weight': 1})
-twitter.add_edge('Rachel', 'Bart', {'weight': 1})
-twitter.add_edge('Tom', 'Bart', {'weight': 1})
-twitter.add_edge('Jack', 'Skye', {'weight': 1})
-twitter.add_edge('Bart', 'Skye', {'weight': 1})
-twitter.add_edge('Rachel', 'Skye', {'weight': 1})
-twitter.add_edge('Greg', 'Bob', {'weight': 1})
-twitter.add_edge('Peter', 'Greg', {'weight': 1})
-twitter.add_edge('Lester', 'Matt', {'weight': 1})
-twitter.add_edge('Max', 'Matt', {'weight': 1})
-twitter.add_edge('Rachel', 'Linda', {'weight': 1})
-twitter.add_edge('Tom', 'Linda', {'weight': 1})
-twitter.add_edge('Bart', 'Greg', {'weight': 1})
-twitter.add_edge('Tom', 'Greg', {'weight': 1})
-twitter.add_edge('Peter', 'Lester', {'weight': 1})
-twitter.add_edge('Tom', 'Mike', {'weight': 1})
-twitter.add_edge('Rachel', 'Mike', {'weight': 1})
-twitter.add_edge('Rachel', 'Glenn', {'weight': 1})
+twitter.add_edge('Rob', 'Rory', {'Weight': 1})
+twitter.add_edge('Markus', 'Rory', {'Weight': 1})
+twitter.add_edge('Markus', 'Rob', {'Weight': 5})
+twitter.add_edge('Mike', 'Rory', {'Weight': 1})
+twitter.add_edge('Mike', 'Rob', {'Weight': 1})
+twitter.add_edge('Mike', 'Markus', {'Weight': 1})
+twitter.add_edge('Mike', 'Linda', {'Weight': 5})
+twitter.add_edge('Alex', 'Rob', {'Weight': 1})
+twitter.add_edge('Alex', 'Markus', {'Weight': 1})
+twitter.add_edge('Alex', 'Mike', {'Weight': 1})
+twitter.add_edge('Glenn', 'Rory', {'Weight': 1})
+twitter.add_edge('Glenn', 'Rob', {'Weight': 1})
+twitter.add_edge('Glenn', 'Markus', {'Weight': 1})
+twitter.add_edge('Glenn', 'Linda', {'Weight': 2})
+twitter.add_edge('Glenn', 'Mike', {'Weight': 1})
+twitter.add_edge('Glenn', 'Alex', {'Weight': 1})
+twitter.add_edge('Richard', 'Rob', {'Weight': 1})
+twitter.add_edge('Richard', 'Linda', {'Weight': 1})
+twitter.add_edge('Richard', 'Mike', {'Weight': 1})
+twitter.add_edge('Richard', 'Alex', {'Weight': 1})
+twitter.add_edge('Richard', 'Glenn', {'Weight': 1})
+twitter.add_edge('Jackie', 'Linda', {'Weight': 1})
+twitter.add_edge('Jackie', 'Mike', {'Weight': 1})
+twitter.add_edge('Jackie', 'Glenn', {'Weight': 1})
+twitter.add_edge('Tom', 'Rachel', {'Weight': 5})
+twitter.add_edge('Rachel', 'Bart', {'Weight': 1})
+twitter.add_edge('Tom', 'Bart', {'Weight': 2})
+twitter.add_edge('Jack', 'Skye', {'Weight': 1})
+twitter.add_edge('Bart', 'Skye', {'Weight': 1})
+twitter.add_edge('Rachel', 'Skye', {'Weight': 1})
+twitter.add_edge('Greg', 'Bob', {'Weight': 1})
+twitter.add_edge('Peter', 'Greg', {'Weight': 1})
+twitter.add_edge('Lester', 'Matt', {'Weight': 1})
+twitter.add_edge('Max', 'Matt', {'Weight': 1})
+twitter.add_edge('Rachel', 'Linda', {'Weight': 1})
+twitter.add_edge('Tom', 'Linda', {'Weight': 1})
+twitter.add_edge('Bart', 'Greg', {'Weight': 2})
+twitter.add_edge('Tom', 'Greg', {'Weight': 2})
+twitter.add_edge('Peter', 'Lester', {'Weight': 2})
+twitter.add_edge('Tom', 'Mike', {'Weight': 1})
+twitter.add_edge('Rachel', 'Mike', {'Weight': 1})
+twitter.add_edge('Rachel', 'Glenn', {'Weight': 1})
+twitter.add_edge('Lester', 'Max', {'Weight': 1})
+twitter.add_edge('Matt', 'Peter', {'Weight': 1})
 
 # add relationship
 twitter['Rob']['Rory']['relationship'] = 'friend'
@@ -81,7 +84,6 @@ twitter['Mike']['Markus']['relationship'] = 'friend'
 twitter['Mike']['Linda']['relationship'] = 'spouse'
 twitter['Alex']['Rob']['relationship'] = 'friend'
 twitter['Alex']['Markus']['relationship'] = 'friend'
-twitter['Alex']['Linda']['relationship'] = 'friend'
 twitter['Alex']['Mike']['relationship'] = 'friend'
 twitter['Glenn']['Rory']['relationship'] = 'friend'
 twitter['Glenn']['Rob']['relationship'] = 'friend'
@@ -115,15 +117,17 @@ twitter['Peter']['Lester']['relationship'] = 'generation'
 twitter['Tom']['Mike']['relationship'] = 'friend'
 twitter['Rachel']['Mike']['relationship'] = 'friend'
 twitter['Rachel']['Glenn']['relationship'] = 'friend'
+twitter['Lester']['Max']['relationship'] = 'friend'
+twitter['Matt']['Peter']['relationship'] = 'friend'
 
 # add posts
 twitter.node['Rory']['posts'] = 182
 twitter.node['Rob']['posts'] = 111
 twitter.node['Markus']['posts'] = 159
 twitter.node['Linda']['posts'] = 128
-twitter.node['Mike']['posts'] = 34
+twitter.node['Mike']['posts'] = 289
 twitter.node['Alex']['posts'] = 188
-twitter.node['Glenn']['posts'] = 45
+twitter.node['Glenn']['posts'] = 252
 twitter.node['Richard']['posts'] = 106
 twitter.node['Jackie']['posts'] = 138
 twitter.node['Skye']['posts'] = 78
@@ -144,6 +148,12 @@ print('\nNodes with data: ', twitter.nodes(data=True))
 
 # print edges
 print('\nEdges with data: ', twitter.edges(data=True))
+
+# graph's density and centrality
+print('\nDensity of the graph: ', nx.density(twitter))
+
+centrality = sorted(alg.centrality.degree_centrality(twitter).items(), key=lambda e: e[1], reverse=True)
+print('\nCentrality of nodes: ', centrality)
 
 # draw the graph
 nx.draw_networkx(twitter)
