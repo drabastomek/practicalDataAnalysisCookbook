d47db2b49c6b9109188882313d84392148b403d8
Tomek Drabas <drabas.t@gmail.com>
2016-01-04 20:33:42 -0800

    Read an article from web added

--- Codes/Chapter9/nlp_read.py
+++ Codes/Chapter9/nlp_read.py
@@ -1,1 +1,33 @@
-import nltk as nl
+import urllib.request as u
+import bs4 as bs
+
+# link to the article at Seattle Times
+nyt_url = 'http://www.seattletimes.com/nation-world/obama-starts-2016-with-a-fight-over-gun-control/'
+
+# read the contents of the webpage
+with u.urlopen(nyt_url) as response:
+    html = response.read()
+
+# using beautiful soup -- let's parse the content of the HTML
+read = bs.BeautifulSoup(html, 'html5lib')
+
+# extract only the parts of interest to us
+body_of_text = []
+
+# get the tile
+body_of_text.append(read.title.get_text())
+
+# find all the paragraphs of the article
+all_ps = read.find_all('p')
+for p in all_ps:
+    body_of_text.append(p.get_text())
+
+# we don't need some of the parts at the bottom of the page
+body_of_text = body_of_text[:24]
+
+# let's see what we got
+print('\n'.join(body_of_text))
+
+# and save it to a file
+with open('../../Data/Chapter9/ST_gunLaws.txt', 'w') as f:
+    f.write('\n'.join(body_of_text))
