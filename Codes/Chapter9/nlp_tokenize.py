34d4a685f75d50bc03b1c1aeb5339cfe2e276a88
Tomek Drabas <drabas.t@gmail.com>
2016-01-04 22:09:28 -0800

    Adding downloader

--- Codes/Chapter9/nlp_tokenize.py
+++ Codes/Chapter9/nlp_tokenize.py
@@ -1,1 +1,18 @@
-import nltk as nl
+import nltk
+
+guns_laws = '../../Data/Chapter9/ST_gunLaws.txt'
+
+with open(guns_laws, 'r') as f:
+    article = f.read()
+
+# print(article)
+
+# load sentence tokenizer
+sentencer = nl.data.load('tokenizers/punkt/english.pickle')
+
+sentences = sentencer.tokenize(article)
+
+tokenizer = nltk.StanfordTokenizer()
+
+for sentence in sentences[1:5]:
+    print(sentence, '\n')
