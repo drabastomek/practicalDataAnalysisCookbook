fe067e273ec92e44d3779364e127d20915d334d7
Tomek Drabas <drabas.t@gmail.com>
2016-01-04 22:37:01 -0800

    Checking in normalization and tokenization

--- Codes/Chapter9/nlp_tokenize.py
+++ Codes/Chapter9/nlp_tokenize.py
@@ -5,14 +5,47 @@ guns_laws = '../../Data/Chapter9/ST_gunLaws.txt'
 with open(guns_laws, 'r') as f:
     article = f.read()
 
-# print(article)
+# load NLTK modules
+sentencer = nltk.data.load('tokenizers/punkt/english.pickle')
+tokenizer = nltk.RegexpTokenizer(r'\w+')
+stemmer = nltk.PorterStemmer()
+lemmatizer = nltk.WordNetLemmatizer()
 
-# load sentence tokenizer
-sentencer = nl.data.load('tokenizers/punkt/english.pickle')
-
+# split the text into sentences
 sentences = sentencer.tokenize(article)
+words = []
+stemmed_words = []
+lemmatized_words = []
 
-tokenizer = nltk.StanfordTokenizer()
+# and for each sentence
+for sentence in sentences:
+    # split the sentence into words
+    words.append(tokenizer.tokenize(sentence))
 
-for sentence in sentences[1:5]:
-    print(sentence, '\n')
+    # stemm the words
+    stemmed_words.append([stemmer.stem(word) 
+        for word in words[-1]])
+
+    # and lemmatize them
+    lemmatized_words.append([lemmatizer.lemmatize(word) 
+        for word in words[-1]])
+
+# and save the results to files
+file_words  = '../../Data/Chapter9/ST_gunLaws_words.txt'
+file_stems  = '../../Data/Chapter9/ST_gunLaws_stems.txt'
+file_lemmas = '../../Data/Chapter9/ST_gunLaws_lemmass.txt'
+
+with open(file_words, 'w') as f:
+    for w in words:
+        for word in w:
+            f.write(word + '\n')
+
+with open(file_stems, 'w') as f:
+    for w in stemmed_words:
+        for word in w:
+            f.write(word + '\n')
+
+with open(file_lemmas, 'w') as f:
+    for w in lemmatized_words:
+        for word in w:
+            f.write(word + '\n')
